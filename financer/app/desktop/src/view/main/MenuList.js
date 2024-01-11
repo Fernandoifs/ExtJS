508f02839ed1c17f6cd1adbcab7a04278d550836
Fernando-Abase <idelmar.souza@abase.com.br>
2024-01-11 17:28:12 -0300

    menu lista

---
+++ financer/app/desktop/src/view/main/MenuList.js
@@ -0,0 +1,5 @@
+Ext.define('Financer.view.main.MenuList',{    
+    extend: 'Ext.list.Tree',
+    alias: 'widget.menulist',
+   
+});
--- financer/app/desktop/src/view/main/NavPanel.js
+++ financer/app/desktop/src/view/main/NavPanel.js
@@ -1,5 +1,12 @@
 Ext.define('Financer.view.main.NavPanel',{    
     extend: 'Ext.Panel',
     alias: 'widget.navpanel',
-    title: 'Nav Panel'
+    title: 'Nav Panel',
+
+    layout: 'fit',
+    shadow: true,
+    margin: '0 5 0 0',
+    items: [{
+        xtype: 'menulist',
+    }]
 });
