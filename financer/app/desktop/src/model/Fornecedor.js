af118023158acad2e19fa48e0049d70d429140f6
Fernando-Abase <idelmar.souza@abase.com.br>
2024-01-16 08:01:18 -0300

    store

--- financer/app/desktop/src/model/Fornecedor.js
+++ financer/app/desktop/src/model/Fornecedor.js
@@ -2,36 +2,13 @@ Ext.define('Financer.model.Fornecedor', {
     extend: 'Financer.model.Base',
 
     idProperty: 'id',
-
-    // proxy: {
-    //     type: 'ajax', 
-    //     url: '/resources/desktop/fornecedores.json',
-    //     reader: {
-    //         type: 'json',
-    //         rootProperty: 'data'
-    //     },
-    //     writer: {
-    //         type: 'json'
-    //     }
-    // },
-
+    
     requires: [
         'Ext.data.validator.Presence',
         'Ext.data.proxy.LocalStorage',
         'Ext.data.proxy.Rest',
         'Ext.data.proxy.Ajax',
     ],
-    // proxy: {
-    //     type: 'rest',
-    //     url: '/store/data.json',
-    //     reader: {
-    //         type: 'json',
-    //         rootProperty: 'data'
-    //     },
-    //     writer: {
-    //         type: 'json'
-    //     }
-    // },
 
     validators: {
         nome: 'presence'
