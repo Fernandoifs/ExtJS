e2fef651a4566aee6137ecc2d757d3bb0f7742f2
Fernandoifs <129119566+Fernandoifs@users.noreply.github.com>
2024-01-16 22:00:11 -0300

    16.01

--- financer/app/desktop/src/view/fornecedor/Main.js
+++ financer/app/desktop/src/view/fornecedor/Main.js
@@ -1,22 +1,22 @@
-// Ext.define('Financer.view.fornecedor.Main', {
-//     extend: 'Ext.Panel',
-//     alias: 'widget.fornecedoresview',
-//     bodyPadding: 0,
+Ext.define('Financer.view.fornecedor.Main', {
+    extend: 'Ext.Panel',
+    alias: 'widget.fornecedoresview',
+    bodyPadding: 0,
 
-//     viewModel: {
-//         type: 'fornecedorview'
-//     },
+    viewModel: {
+        type: 'fornecedorview'
+    },
     
-//     layout: {
-//         type: 'vbox',
-//         align: 'stretch'
-//     },
-//     items: [{
-//         xtype: 'fornecedorgrid',
-//         flex: 1,
-//         shadow: true,
-//         bind: {
-//             store: '{fornecedores}'
-//         }
-//     }]
-// })
+    layout: {
+        type: 'vbox',
+        align: 'stretch'
+    },
+    items: [{
+        xtype: 'fornecedorgrid',
+        flex: 1,
+        shadow: true,
+        bind: {
+            store: '{fornecedores}'
+        }
+    }]
+})
